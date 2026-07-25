59f4b4a7ad9c4ffbb4e1accf4144b2191ca37184
juegoserod <ardinoeros@icloud.com>
2026-07-24 22:00:52 -0300

    Crear preguntas.js

---
+++ preguntas.js
@@ -0,0 +1,83 @@
+export const preguntasDefault = [
+
+"¿Quién tiene más probabilidades de desaparecer si gana la lotería?",
+
+"¿Quién sería el peor compañero de viaje?",
+
+"¿Quién sería el primero en llegar tarde a su propio casamiento?",
+
+"¿Quién tiene el historial de búsqueda más raro?",
+
+"¿Quién sería el primero en vender a un amigo por plata?",
+
+"¿Quién tiene más chances de terminar viral por vergüenza?",
+
+"¿Quién es el más chamuyero del grupo?",
+
+"¿Quién sería el peor presidente del país?",
+
+"¿Quién tiene más probabilidades de mandar un mensaje al chat equivocado?",
+
+"¿Quién sería el primero en ir preso por una boludez?",
+
+"¿Quién tiene menos paciencia del grupo?",
+
+"¿Quién sería el peor jefe?",
+
+"¿Quién sobreviviría menos tiempo en una isla desierta?",
+
+"¿Quién tiene más probabilidades de romper algo ajeno y hacerse el distraído?",
+
+"¿Quién sería el primero en quedarse dormido en una fiesta?",
+
+"¿Quién es el más competitivo?",
+
+"¿Quién sería el primero en abandonar un desafío?",
+
+"¿Quién tiene más posibilidades de olvidar un cumpleaños?",
+
+"¿Quién sería el peor detective?",
+
+"¿Quién es el más caradura?",
+
+"¿Quién tiene más chances de ser cancelado en redes?",
+
+"¿Quién sería el peor profesor?",
+
+"¿Quién sería el primero en perder el celular?",
+
+"¿Quién es el peor mintiendo?",
+
+"¿Quién sería el primero en gastar toda la plata apenas cobra?",
+
+"¿Quién tiene más probabilidades de mudarse a otro país sin avisar?",
+
+"¿Quién sería el peor compañero de trabajo?",
+
+"¿Quién es el más dramático?",
+
+"¿Quién sería el primero en renunciar por enojo?",
+
+"¿Quién tiene más probabilidades de quedarse sin batería siempre?",
+
+"¿Quién es el más desordenado?",
+
+"¿Quién sería el primero en romper un secreto?",
+
+"¿Quién tiene más suerte?",
+
+"¿Quién tiene peor suerte?",
+
+"¿Quién sería el primero en quedarse afuera de su casa?",
+
+"¿Quién es el más insoportable cuando tiene razón?",
+
+"¿Quién sería el peor cocinero?",
+
+"¿Quién tiene más probabilidades de ganar un reality?",
+
+"¿Quién sería el peor abogado?",
+
+"¿Quién es el más vago del grupo?"
+
+];
